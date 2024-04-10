649f54652dd2f36575d99c5d48d1e4e34e9b05da
Aruna45920 <arunakumarinaramreddy@gmail.com>
2024-04-10 21:57:49 +0530

    second commit

--- Demo/src/com/bank/Pattern.java
+++ Demo/src/com/bank/Pattern.java
@@ -1,29 +1,13 @@
+/*
+1
+01
+101
+0101
+10101
+ */
 package com.bank;
 
-
-
 public class Pattern {
-
-
-    public static void main(String[] args) {
-
-        int n = 10;
-        int m=13;
-
-        for (int i =0; i <m; i++) {
-            for (int j = 0; j <n; j++) {
-                if (i == 0 || i==m-1) {
-                    System.out.print("* ");
-                } else {
-                    if(i%4==0||j==0||j==n-1||j%3==0) {
-                        System.out.print("* ");
-                    } else {
-                        System.out.print("  ");
-                    }
-
-                }
-            }
-            System.out.println();
-        }
+   public 
     }
 }
